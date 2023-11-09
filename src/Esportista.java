8188f3a868b96616ddfa1384f9059a48b54ddc7e
lecoTV <lecotv2006123@gmail.com>
2023-11-09 10:00:30 -0300

    aulas y aulas

---
+++ src/Esportista.java
@@ -0,0 +1,19 @@
+public class Esportista extends Pessoa {
+    private int idFederacao;
+
+    // Construtores
+
+    public Esportista(int idFederacao) {
+        this.idFederacao = idFederacao;
+    }
+
+    public Esportista() {
+    }
+
+    // método
+    public String sacar(){
+        return this.getNome() + " Saque Viagem...";
+    }
+
+
+}
